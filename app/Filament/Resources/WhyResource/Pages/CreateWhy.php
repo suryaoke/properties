f6e1c6e7d22185384b4f0934d0125d22f8281335
Surya Sahputra <89033805+suryaoke@users.noreply.github.com>
2025-09-25 09:48:38 +0700

    Update CreateWhy.php

--- app/Filament/Resources/WhyResource/Pages/CreateWhy.php
+++ app/Filament/Resources/WhyResource/Pages/CreateWhy.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateWhy extends CreateRecord
 {
     protected static string $resource = WhyResource::class;
+   protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
