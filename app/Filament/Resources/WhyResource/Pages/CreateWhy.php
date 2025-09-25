dfe50acfc87c17180dd374da52f62d8f044cfed8
Surya Sahputra <89033805+suryaoke@users.noreply.github.com>
2025-09-25 09:37:28 +0700

    Create CreateWhy.php

---
+++ app/Filament/Resources/WhyResource/Pages/CreateWhy.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\WhyResource\Pages;
+
+use App\Filament\Resources\WhyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateWhy extends CreateRecord
+{
+    protected static string $resource = WhyResource::class;
+}
