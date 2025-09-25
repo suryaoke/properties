e001079729ab82c2dc12c9fd1a40d100705a73e7
Surya Sahputra <89033805+suryaoke@users.noreply.github.com>
2025-09-25 09:34:14 +0700

    Add files via upload

---
+++ app/Filament/Resources/WhyResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\WhyResource\Pages;
+use App\Filament\Resources\WhyResource\RelationManagers;
+use App\Models\Why;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class WhyResource extends Resource
+{
+    protected static ?string $model = Why::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListWhies::route('/'),
+            'create' => Pages\CreateWhy::route('/create'),
+            'edit' => Pages\EditWhy::route('/{record}/edit'),
+        ];
+    }
+}
