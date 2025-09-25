b46a12a48a0120b4213773c4369ddae793697586
Surya Sahputra <89033805+suryaoke@users.noreply.github.com>
2025-09-25 09:52:29 +0700

    Update WhyResource.php

--- app/Filament/Resources/WhyResource.php
+++ app/Filament/Resources/WhyResource.php
@@ -21,7 +21,7 @@ class WhyResource extends Resource
     protected static ?string $navigationLabel = 'Why'; // ubah nama di menu
     protected static ?string $pluralLabel = 'Why'; // judul di list
     protected static ?string $label = 'Why';
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
 
     public static function form(Form $form): Form
     {
