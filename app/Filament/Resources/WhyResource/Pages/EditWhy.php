0140dbf0fd545d650dfca3dd30c559f68553c45c
Surya Sahputra <89033805+suryaoke@users.noreply.github.com>
2025-09-25 09:48:56 +0700

    Update EditWhy.php

--- app/Filament/Resources/WhyResource/Pages/EditWhy.php
+++ app/Filament/Resources/WhyResource/Pages/EditWhy.php
@@ -9,11 +9,15 @@ use Filament\Resources\Pages\EditRecord;
 class EditWhy extends EditRecord
 {
     protected static string $resource = WhyResource::class;
+     protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            //Actions\DeleteAction::make(),
         ];
     }
 }
