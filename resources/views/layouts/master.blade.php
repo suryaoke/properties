ee6d181ff723e1d28ea1bb5b71459d679cca59b8
Surya Sahputra <89033805+suryaoke@users.noreply.github.com>
2025-09-27 15:31:18 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,47 +1,56 @@
 <!doctype html>
 <html>
-    <head>
-        <title>@yield('title')</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500"> 
-        <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css') }}">
 
-        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{asset('css/magnific-popup.css') }}">
-        <link rel="stylesheet" href="{{asset('css/jquery-ui.css') }}">
-        <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css') }}">
-        <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css') }}">
-        <link rel="stylesheet" href="{{asset('css/mediaelementplayer.css') }}">
-        <link rel="stylesheet" href="{{asset('css/animate.css') }}">
-        <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css') }}">
-        <link rel="stylesheet" href="{{asset('css/fl-bigmug-line.css') }}">
-        
-    
-        <link rel="stylesheet" href="{{asset('css/aos.css') }}">
+<head>
+    <title>@yield('title')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @if (!empty($about->photo))
+        <link rel="icon" type="image/png" href="{{ Storage::url($about->photo) }}">
+    @else
+        <link rel="icon" type="image/png" href="{{ asset('images/default-favicon.png') }}">
+    @endif
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500">
+    <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
 
-        <link rel="stylesheet" href="{{asset('css/style.css') }}">
-    </head>
-    <body>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mediaelementplayer.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/flaticon/font/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fl-bigmug-line.css') }}">
 
-        @yield('content')
-       
 
-      
-        <script src="{{asset('js/jquery-3.3.1.min.js') }}"></script>
-        <script src="{{asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
-        <script src="{{asset('js/jquery-ui.js') }}"></script>
-        <script src="{{asset('js/popper.min.js') }}"></script>
-        <script src="{{asset('js/bootstrap.min.js') }}"></script>
-        <script src="{{asset('js/owl.carousel.min.js') }}"></script>
-        <script src="{{asset('js/mediaelement-and-player.min.js') }}"></script>
-        <script src="{{asset('js/jquery.stellar.min.js') }}"></script>
-        <script src="{{asset('js/jquery.countdown.min.js') }}"></script>
-        <script src="{{asset('js/jquery.magnific-popup.min.js') }}"></script>
-        <script src="{{asset('js/bootstrap-datepicker.min.js') }}"></script>
-        <script src="{{asset('js/aos.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
 
-        <script src="{{asset('js/main.js') }}"></script>  
-    </body>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+
+    @yield('content')
+
+
+
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-ui.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/mediaelement-and-player.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('js/aos.js') }}"></script>
+
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
 </html>
